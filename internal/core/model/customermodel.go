e7c04774bc4d26378320e8080e2c4af28ee3f86f
Peter Winkelmans <peter.winkelmans@sennit.be>
2024-06-13 20:04:49 +0200

    update

--- internal/core/model/customermodel.go
+++ internal/core/model/customermodel.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"errors"
+	"strings"
+
 	"github.com/google/uuid"
 )
 
@@ -17,7 +20,24 @@ type Customer struct {
 	CountryCode string
 }
 
+var (
+	errorNoName           = errors.New("Name field is empty")
+	errorNoAddress        = errors.New("Address field is empty")
+	errorWrongPostcode    = errors.New("Incorrect postcode")
+	errorNoCommunity      = errors.New("Community field is empty")
+	errorWrongCountryCode = errors.New("Country code is not correct")
+)
+
 func NewCustomer(name string, address string, postcode int, community string, countrycode string) (*Customer, error) {
+	// --- verification of the customer
+	name = strings.TrimSpace(name)
+	if name == "" {
+		return nil, errorNoName
+	}
+
+	address = strings.TrimSpace(address)
+
+	// --- Creation of the customer
 	customer := Customer{
 		ID:          uuid.New().string(),
 		Name:        name,
